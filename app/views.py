ef8bd5bb9ea5a8cb1d146dfb0495da8562bdcb1e
bansi2007 <170303108038@paruluniversity.ac.in>
2021-02-16 15:57:59 +0530

    filtered according to dates

--- app/views.py
+++ app/views.py
@@ -46,30 +46,31 @@ def pages(request):
 
 def version_matrix(request): 
     data =  [ {"date":"10-11-2020","service":"service_abc", "environment":"1.0", "annotation":"OK"},
-    {"date":"10-11-2020","service":"service_vwx", "environment":"1.0", "annotation":"VersionMismatch"},
-    {"date":"10-11-2020","service":"service_xyz", "environment":"1.2", "annotation":"VersionMismatch"},
-    {"date":"10-12-2020","service":"service_yz", "environment":"1.6", "annotation":"OK"},
-    {"date":"10-11-2020","service":"service_def", "environment":"1.9", "annotation":"VersionMismatch"},
-    {"date":"10-11-2020","service":"service_123", "environment":"1.0", "annotation":"OK"},
-    {"date":"10-16-2020","service":"service_ghi", "environment":"1.6", "annotation":"OK"},
-    {"date":"10-16-2020","service":"service_456", "environment":"1.4", "annotation":"VersionMismatch"},
-    {"date":"10-16-2020","service":"service_789", "environment":"1.8", "annotation":"OK"},
-    {"date":"10-11-2020","service":"service_ABC", "environment":"1.0", "annotation":"OK"},
-    {"date":"10-11-2020","service":"service_DEF", "environment":"2.6", "annotation":"OK"},
-    {"date":"10-16-2020","service":"service_GHI", "environment":"1.6", "annotation":"VersionMismatch"},
-    {"date":"10-16-2020","service":"service_JKL", "environment":"2.0", "annotation":"OK"},
-    {"date":"10-09-2020","service":"service_jkl", "environment":"1.2", "annotation":"OK"},
-    {"date":"10-09-2020","service":"service_mno", "environment":"1.8", "annotation":"VersionMismatch"},
-    {"date":"10-09-2020","service":"service_pqr", "environment":"1.6", "annotation":"OK"},
-    {"date":"10-09-2020","service":"service_stu", "environment":"17", "annotation":"OK"}
+    {"date":"11-10-2021","service":"service_vwx", "environment":"1.0", "annotation":"VersionMismatch"},
+    {"date":"11-10-2021","service":"service_xyz", "environment":"1.2", "annotation":"VersionMismatch"},
+    {"date":"12-10-2021","service":"service_yz", "environment":"1.6", "annotation":"OK"},
+    {"date":"11-10-2021","service":"service_def", "environment":"1.9", "annotation":"VersionMismatch"},
+    {"date":"11-10-2021","service":"service_123", "environment":"1.0", "annotation":"OK"},
+    {"date":"16-10-2021","service":"service_ghi", "environment":"1.6", "annotation":"OK"},
+    {"date":"16-10-2021","service":"service_456", "environment":"1.4", "annotation":"VersionMismatch"},
+    {"date":"16-10-2021","service":"service_789", "environment":"1.8", "annotation":"OK"},
+    {"date":"11-11-2021","service":"service_ABC", "environment":"1.0", "annotation":"OK"},
+    {"date":"10-11-2021","service":"service_DEF", "environment":"2.6", "annotation":"OK"},
+    {"date":"11-10-2021","service":"service_GHI", "environment":"1.6", "annotation":"VersionMismatch"},
+    {"date":"11-10-2021","service":"service_JKL", "environment":"2.0", "annotation":"OK"},
+    {"date":"09-09-2021","service":"service_jkl", "environment":"1.2", "annotation":"OK"},
+    {"date":"09-09-2021","service":"service_mno", "environment":"1.8", "annotation":"VersionMismatch"},
+    {"date":"09-09-2021","service":"service_pqr", "environment":"1.6", "annotation":"OK"},
+    {"date":"09-09-2021","service":"service_stu", "environment":"17", "annotation":"OK"}
     ]
-    # seldate = request.GET['data']
-    # blacklist = []
-    # for i in data['date'] :
-    #     if seldate == i :
-    #         i.append(blacklist)
-    
-    return HttpResponse(json.dumps({'data':data}), content_type = 'applications/json')
+
+    seldate = request.GET['data']
+    print(seldate)
+    blacklist = []
+    for dates in data:
+        if seldate == dates["date"] :
+            blacklist.append(dates)
+    return HttpResponse(json.dumps({'data':blacklist}), content_type = 'applications/json')
 
    
 
